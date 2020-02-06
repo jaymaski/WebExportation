e2fb1fadee173837d122af1c6fd9d18f86e91906
Juan Miguel Roman <nhestroman@gmail.com>
2020-02-07 07:44:41 +0800

    Changed CSS and Header
    Fixed header and refactored CSS

--- application/views/users/users_dashboard.php
+++ application/views/users/users_dashboard.php
@@ -1,1 +1,32 @@
-<div> <?php echo $title ?> </div>
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">NAME</th>
+      <th scope="col">TYPE</th>
+      <th scope="col">ENVIRONMENT</th>
+      <th scope="col">EXPORTATION DATE</th>
+      <th scope="col">URGENCY</th>
+      <th scope="col">STATUS</th>
+      <th scope="col">QUEUE</th>
+      <th scope="col">ACTIONS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="txt-oflo">B2BE20200201-01</td>
+      <td class="txt-oflo">Lorem AU to V2</td>
+      <td class="txt-oflo">Process Model</td>
+      <td class="txt-oflo">UAT</td>
+      <td class="txt-oflo">02/01/2020 4:00PM</td>
+      <td class="txt-oflo">NORMAL</td>
+      <td class="txt-oflo">IN QUEUE</td>
+      <td class="txt-oflo">45</td >
+      <td class="txt-oflo">
+          <button class="btn btn-success btn-sm" onclick="$('#modal-summary').modal('show');return false;"> view </button>
+          <button class="btn btn-info btn-sm" onclick="$('#modal-summary').modal('show');return false;"> edit </button>
+        <button class="btn btn-warning btn-sm" onclick="$('#modal-summary').modal('show');return false;"> share </button>
+      </td>
+    </tr>
+  </tbody>
+</table>
