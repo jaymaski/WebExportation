adc4f7f52bab0c7ad38fbf679fcbbb3ca3bb9e81
Jhom <cruzjoemarie123@gmail.com>
2020-02-13 10:34:15 +0800

    Updated my_uat_prod_queue_exported page

--- application/views/users/dashboards/my_uatexported.php
+++ application/views/users/dashboards/my_uatexported.php
@@ -1,36 +1,49 @@
 <div class="tab-pane fade" id="nav-my-uat-exported" role="tabpanel" aria-labelledby="nav-my-uat-exported-tab">
-                        <div class="table-responsive">
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>NAME</th>
-                                        <th>TYPE</th>
-                                        <th>ENVIRONMENT</th>
-                                        <th>EXPORTATION DATE</th>
-                                        <th>URGENCY</th>
-                                        <th>STATUS</th>
-                                        <th>QUEUE</th>
-                                        <th>ACTIONS</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td class="txt-oflo">B2BE20200201-06</td>
-                                        <td class="txt-oflo">translation1</td>
-                                        <td class="txt-oflo">Translation</td>
-                                        <td class="txt-oflo">UAT EXPORTED</td>
-                                        <td class="txt-oflo">02/01/2020 4:00PM</td>
-                                        <td class="txt-oflo">NORMAL</td>
-                                        <td class="txt-oflo">IN QUEUE</td>
-                                        <td class="txt-oflo">56</td>
-                                        <td class="txt-oflo">
-                                            <button class="btn btn-success btn-sm" onclick="$('#modal-summary').modal('show');return false;"> view </button>
-                                            <button class="btn btn-info btn-sm" onclick="$('#modal-summary').modal('show');return false;"> edit </button>
-                                            <button class="btn btn-warning btn-sm" onclick="$('#modal-summary').modal('show');return false;"> share </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+<?php if($my_requests){?>
+	<div class="table-responsive">
+		<table class="table-hover table dashboard">
+			<thead>
+				<tr>
+					<th id="request-id">ID</th>
+					<th id="request-name">NAME</th>
+					<th id="requestor-name">OWNER</th>
+					<th id="urgency">URGENCY</th>
+					<th id="request-date">EXPORTATION DATE</th>
+					<th id="environment">ENVIRONMENT</th>
+					<th id="status">STATUS</th>
+					<th id="actions">ACTIONS</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 	$counter = 0;
+						foreach($my_requests as $request){
+							if($request->environment == "UAT"){
+								if($request->status == "Exported"){
+				?>
+				<tr>
+					<td class="txt-oflo" id="request-id"><?php echo $request->requestID ;?></td>
+					<td class="txt-oflo" id="request-name">PROD_CR-csremail-au-wiscust-au-PO(B2BE#3893292)</td>
+					<td class="txt-oflo" id="requestor-name"><?php echo $request->owner ;?></td>
+					<td class="txt-oflo" id="urgency">NORMAL</td>
+					<td class="txt-oflo" id="request-date"><?php echo $request->requestDate ;?></td>
+					<td class="txt-oflo" id="environment"><?php echo $request->environment ;?></td>
+					<td class="txt-oflo" id="status"><?php echo $request->status ;?></td>
+					<td class="txt-oflo" id="actions" >
+						<button class="btn btn-success btn-sm" onclick="window.location.replace('<?php echo site_url('request/view_request').'/'.$request->projectID.'/'.$request->taskID.'/'.$request->requestID; ?>');"> view </button>
+						<button class="btn btn-info btn-sm" onclick="$('#modal-summary').modal('show');return false;"> edit </button>
+						<button class="btn btn-warning btn-sm" onclick="$('#modal-summary').modal('show');return false;"> share </button>
+					</td>
+				</tr>
+				<?php 	$counter += 1	;	
+								}
+							}
+						} ?>
+				
+			</tbody>
+		</table>
+		<?php	if($counter == 0){ ?>
+							<p>No Exported UAT request found.</p>
+		<?php	}	?>
+	</div>
+</div>
+<?php 	} ?>
