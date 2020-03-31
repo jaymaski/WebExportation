bf4687a3817ff2c72644c5ce73012ea3c703d8ef
Jay <jjaymascarenas@gmail.com>
2020-03-31 10:32:58 +0800

    Fix Footer

--- application/views/Template/footer_main.php
+++ application/views/Template/footer_main.php
@@ -9,68 +9,25 @@
 <!-- jQuery Custom Scroller CDN -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
-    <script>
-        ClassicEditor
-        .create(document.querySelector('#richTextEditor'), {
-            toolbar: [ 'heading', '|', 'bold', 'italic', 'link', '|', 'bulletedList', 'numberedList', 'blockQuote', 'codeBlock' ],
-            heading: {
-                options: [
-                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                ]
-            },
-            codeBlock: {
-                
-            }  
-        })
-        .catch( error => {
-            //console.error( error );
-        });
-    </script>
-
-    <script type="text/javascript">
-        function view_project(projectID, taskID, requestID) {        
-            var projectID = projectID;
-            var taskID = taskID;
-            var requestID = requestID;
-            var json = {'projectID':'" + projectID + "','taskID':'" + taskID + "','requestID':'" + requestID + "'};
-            $.ajax({
-                type:'POST',
-                url:"<?php echo site_url('request/view_request'); ?>/" + projectID + "/" + taskID + "/" + requestID,
-                dataType: 'json',
-                data: json,
-                success:function(data) {
-                    // console.log(data.requests);
-                    // console.log(data.curr_request);
-                    // console.log(data.translation_changes);
-                    console.log(data.translations);
-                    console.log(data.impacted);
-
-                    //Project Details
-                    document.getElementById("projectID").innerHTML = data.curr_request[0]['projectID'];
-                    document.getElementById("taskID").innerHTML = data.curr_request[0]['taskID'];
-                    document.getElementById("projectOwner").innerHTML = data.curr_request[0]['projectOwner'];
-                    document.getElementById("docType").innerHTML = data.curr_request[0]['docType'];
-                    document.getElementById("sender").innerHTML = data.curr_request[0]['sender'];
-                    document.getElementById("receiver").innerHTML = data.curr_request[0]['receiver'];
-                    
-                    var requestsNum = Object.keys(data.requests).length;
-                    var translationNum = Object.keys(data.translations).length;
-                    var impactedNum = Object.keys(data.impacted).length;
-                    
-                    $( ".revisions:not(:first)").each(function(){
-                        $(this).remove();
-                    });
-                    $( ".translations:not(:first)").each(function(){
-                        $(this).remove();
-                    });
-                    $( ".impacted:not(:first)").each(function(){
-                        $(this).remove();
-                    });
-                    $(".commentSection:not(:first)").each(function(){
-                        $(this).remove();
-                    });                      
+<script type="text/javascript">
+    ClassicEditor
+    .create(document.querySelector('#richTextEditor'), {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', '|', 'bulletedList', 'numberedList', 'blockQuote', 'codeBlock' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        },
+        codeBlock: {
+            
+        }  
+    })
+    .catch( error => {
+        //console.error( error );
+    });
+</script>       
 
 <script type="text/javascript">
     function view_project(projectID, taskID, requestID) {
@@ -264,140 +221,139 @@
             $('#sidebar').removeClass('active');
             $('.overlay').removeClass('active');
         });
-    </script>	
+    });
+</script>	
 
-    <script>
-        function expandActions(){
-            if(window.getComputedStyle(document.getElementById('row-1'),null).getPropertyValue("width") == '0px'){
-                document.getElementById('row-1').style.width = '10%';
-            }    
-            else{
-                document.getElementById('row-1').style.width = '0%';
-            }
-        }
-
-        //init elements
-        var projectID = document.getElementById('projectID');
-        var taskID = document.getElementById('taskID');
-        var projectOwner = document.getElementById('projectOwner');
-        var sender = document.getElementById('sender');
-        var receiver = document.getElementById('receiver');
-        var documentType = document.getElementById('docType');
-
-        //init content holders
-        var projectIDContent =  projectID.innerText;
-        var taskIDContent = taskID.innerText;
-        var projectOwnerContent = projectOwner.innerText;
-        var senderContent = sender.innerText;
-        var receiverContent = receiver.innerText;
-        var documentTypeContent = documentType.innerText;
-
-        //init button listeners
-        var editButton = document.getElementById('edit')
-        var saveButton = document.getElementById('save')
-        var shareButton = document.getElementById('share')
-        var cancelButton = document.getElementById('cancel')        
-
-        //function triggers
-        saveButton.style.display = 'none';
-        cancelButton.style.display = 'none';
-        saveButton.onclick = saveChanges;
-        editButton.onclick = toggleEdit;
-        cancelButton.onclick = toDisplay;
-
-        //main edit
-        function toggleEdit(){
-            var response = confirm('Editing the document will remove you from the queue! Continue?');
-            if (response == true) {
-                var isEdit = false;
-                if(isEdit){
-                    toDisplay();
-                    saveButton.style.display = 'none';
-                    isEdit = false;
-                }
-
-                else{
-                    projectIDContent =  projectID.innerText;
-                    taskIDContent = taskID.innerText;
-                    projectOwnerContent = projectOwner.innerText;
-                    senderContent = sender.innerText;
-                    receiverContent = receiver.innerText;
-                    documentTypeContent = documentType.innerText; 
-
-                    toEdit();
-                    editButton.style.display = 'none';
-                    shareButton.style.display = 'none';
-                    saveButton.style.display = 'inline';
-                    cancelButton.style.display = 'inline';
-                    isEdit = true;
-                }
-            }
-        }
-
-        //save to server and update holders
-        function saveChanges(){
-            projectIDContent = document.getElementById('projectIDInput').value
-            taskIDContent = document.getElementById('taskIDInput').value
-            projectOwnerContent =  document.getElementById('projectOwnerInput').value
-            senderContent =  document.getElementById('senderInput').value
-            receiverContent = document.getElementById('receiverInput').value
-            documentTypeContent =  document.getElementById('documentTypeInput').value
-            
-            toDisplay();
-            isEdit = false;
-
-            $.ajax({
-                type: "POST",
-                url: "<?php echo site_url('request/update') ?>",
-                dataType: "JSON",
-                data: {
-                    projectID: projectIDContent,
-                    taskID: taskIDContent,
-                    projectOwner:projectOwnerContent,
-                    sender:senderContent,
-                    receiver:receiverContent,
-                    docType:documentTypeContent,
-                },
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function(){
-                    alert('failed');
-                }
-            });
-        }
-
-        //toggle textbox
-        function toEdit(){
-            projectID.innerHTML = '<input name=\'projectIDInput\' id=\'projectIDInput\' value='+projectIDContent+ '></input>';
-            taskID.innerHTML = '<input name=\'taskIDInput\' id=\'taskIDInput\' value='+taskIDContent+ '></input>';
-            projectOwner.innerHTML = '<input name=\'projectOwnerInput\' id=\'projectOwnerInput\' value='+projectOwnerContent+ '></input>';
-            sender.innerHTML = '<input name=\'senderInput\' id=\'senderInput\' value='+senderContent+ '></input>';
-            receiver.innerHTML = '<input name=\'receiverInput\' id=\'receiverInput\' value='+receiverContent+ '></input>';
-            documentType.innerHTML = '<input name=\'documentTypeInput\' id=\'documentTypeInput\' value='+documentTypeContent+ '></input>';
-        }
-
-        //toggle display
-        function toDisplay(){
-            saveButton.style.display = 'none';
-            editButton.style.display = 'inline';
-            shareButton.style.display = 'inline';
-            cancelButton.style.display = 'none';
-            projectID.innerHTML = projectIDContent;
-            taskID.innerHTML = taskIDContent;
-            projectOwner.innerHTML = projectOwnerContent;
-            sender.innerHTML =senderContent;
-            receiver.innerHTML = receiverContent;
-            documentType.innerHTML =documentTypeContent;
+<script type="text/javascript">
+    function expandActions(){
+        if(window.getComputedStyle(document.getElementById('row-1'),null).getPropertyValue("width") == '0px'){
+            document.getElementById('row-1').style.width = '10%';
         }    
-    </script>
+        else{
+            document.getElementById('row-1').style.width = '0%';
+        }
+    }
+
+    //init elements
+    var projectID = document.getElementById('projectID');
+    var taskID = document.getElementById('taskID');
+    var projectOwner = document.getElementById('projectOwner');
+    var sender = document.getElementById('sender');
+    var receiver = document.getElementById('receiver');
+    var documentType = document.getElementById('docType');
+
+    //init content holders
+    var projectIDContent =  projectID.innerText;
+    var taskIDContent = taskID.innerText;
+    var projectOwnerContent = projectOwner.innerText;
+    var senderContent = sender.innerText;
+    var receiverContent = receiver.innerText;
+    var documentTypeContent = documentType.innerText;
+
+    //init button listeners
+    var editButton = document.getElementById('edit')
+    var saveButton = document.getElementById('save')
+    var shareButton = document.getElementById('share')
+    var cancelButton = document.getElementById('cancel')        
+
+    //function triggers
+    saveButton.style.display = 'none';
+    cancelButton.style.display = 'none';
+    saveButton.onclick = saveChanges;
+    editButton.onclick = toggleEdit;
+    cancelButton.onclick = toDisplay;
+
+    //main edit
+    function toggleEdit(){
+        var response = confirm('Editing the document will remove you from the queue! Continue?');
+        if (response == true) {
+            var isEdit = false;
+            if(isEdit){
+                toDisplay();
+                saveButton.style.display = 'none';
+                isEdit = false;
+            }
+
+            else{
+                projectIDContent =  projectID.innerText;
+                taskIDContent = taskID.innerText;
+                projectOwnerContent = projectOwner.innerText;
+                senderContent = sender.innerText;
+                receiverContent = receiver.innerText;
+                documentTypeContent = documentType.innerText; 
+
+                toEdit();
+                editButton.style.display = 'none';
+                shareButton.style.display = 'none';
+                saveButton.style.display = 'inline';
+                cancelButton.style.display = 'inline';
+                isEdit = true;
+            }
+        }
+    }
+
+    //save to server and update holders
+    function saveChanges(){
+        projectIDContent = document.getElementById('projectIDInput').value
+        taskIDContent = document.getElementById('taskIDInput').value
+        projectOwnerContent =  document.getElementById('projectOwnerInput').value
+        senderContent =  document.getElementById('senderInput').value
+        receiverContent = document.getElementById('receiverInput').value
+        documentTypeContent =  document.getElementById('documentTypeInput').value
+        
+        toDisplay();
+        isEdit = false;
+
+        $.ajax({
+            type: "POST",
+            url: "<?php echo site_url('request/update') ?>",
+            dataType: "JSON",
+            data: {
+                projectID: projectIDContent,
+                taskID: taskIDContent,
+                projectOwner:projectOwnerContent,
+                sender:senderContent,
+                receiver:receiverContent,
+                docType:documentTypeContent,
+            },
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(){
+                alert('failed');
+            }
+        });
+    }
+
+    //toggle textbox
+    function toEdit(){
+        projectID.innerHTML = '<input name=\'projectIDInput\' id=\'projectIDInput\' value='+projectIDContent+ '></input>';
+        taskID.innerHTML = '<input name=\'taskIDInput\' id=\'taskIDInput\' value='+taskIDContent+ '></input>';
+        projectOwner.innerHTML = '<input name=\'projectOwnerInput\' id=\'projectOwnerInput\' value='+projectOwnerContent+ '></input>';
+        sender.innerHTML = '<input name=\'senderInput\' id=\'senderInput\' value='+senderContent+ '></input>';
+        receiver.innerHTML = '<input name=\'receiverInput\' id=\'receiverInput\' value='+receiverContent+ '></input>';
+        documentType.innerHTML = '<input name=\'documentTypeInput\' id=\'documentTypeInput\' value='+documentTypeContent+ '></input>';
+    }
+
+    //toggle display
+    function toDisplay(){
+        saveButton.style.display = 'none';
+        editButton.style.display = 'inline';
+        shareButton.style.display = 'inline';
+        cancelButton.style.display = 'none';
+        projectID.innerHTML = projectIDContent;
+        taskID.innerHTML = taskIDContent;
+        projectOwner.innerHTML = projectOwnerContent;
+        sender.innerHTML =senderContent;
+        receiver.innerHTML = receiverContent;
+        documentType.innerHTML =documentTypeContent;
+    }    
+</script>
 
 <script type="text/javascript">
     var regex = /^(.+?)(\d+)$/i;
     var addTranslationIndex = 1;
-
     function add() {
-
         let template = `             <div id="translation">
 											<div>
 												<table name="revision-translations0">
@@ -541,45 +497,7 @@
             console.log("pasok na :D ");
         }
     });
-  
-
-
-        //if (validate()) {
-        // var formData = {};
-        // $("section").each(function() {
-        //     var section = {};
-
-        //     $(this)
-        //         .find(".input-class")
-        //         .each(function() {
-        //             section[$(this).attr("name")] = $(this).val();
-        //         });
-
-        //     formData[$(this).attr("name")] = section;
-        // });
-        // formData["index"] = cloneIndex;
-        // var jsonData = JSON.stringify(formData);
-        // console.log(jsonData);
-        // console.log(document.querySelectorAll('translation0 #impactedtable').length);
-        // $.ajax({
-        //     type: "POST",
-
-        //     dataType: "JSON",
-        //     data: {
-        //         data: jsonData
-        //     },
-        //     success: function(data) {
-        //         console.log(data);
-        //         console.log("pasok na :D ");
-        //     }
-        // });
-
-
     });
 </script>
-
-
-
 </body>
-
 </html>
